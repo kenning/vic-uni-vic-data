d77130057b54dea5255c0b8548f4904d9d4de301
Nick Thinkpad X1 <hwerzog@gmail.com>
2024-06-07 10:26:28 +1200

    Add back in run_spark.sh

---
+++ run_spark.sh
@@ -0,0 +1,21 @@
+need java
+
+MY_USERNAME=$(whoami)
+hadoop fs -mkdir -p /user/$MY_USERNAME
+hadoop fs -put Gungor_2018_VictorianAuthorAttribution_data.csv /user/$MY_USERNAME/Gungor_2018_VictorianAuthorAttribution_data.csv
+
+# Remove output dir if exists
+hadoop fs -test -d /user/$MY_USERNAME/vic-output && hadoop fs -rm -r /user/$MY_USERNAME/vic-output
+
+echo "Running spark-submit"
+#spark-submit --master yarn --deploy-mode cluster main.py $MY_USERNAME 
+spark-submit --master yarn main.py $MY_USERNAME 
+
+# Print the output (its saved in hdfs)
+hadoop fs -cat /user/$MY_USERNAME/vic-output/logs/part-00000
+echo "--"
+hadoop fs -cat /user/$MY_USERNAME/vic-output/df-0/feat_df_0.csv
+echo "--"
+hadoop fs -cat /user/$MY_USERNAME/vic-output/df-1/feat_df_1.csv
+echo "--"
+hadoop fs -cat /user/$MY_USERNAME/vic-output/df-2/feat_df_2.csv
