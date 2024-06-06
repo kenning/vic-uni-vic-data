3a3455bbfade52fc03728112940c1ace6408184f
Nicholas Hemphill McCormick <mccormnich@co246a-1.ecs.vuw.ac.nz>
2024-06-06 20:21:41 +1200

    Add quickie runspark line

--- run_spark.sh
+++ run_spark.sh
@@ -8,8 +8,8 @@ hadoop fs -put Gungor_2018_VictorianAuthorAttribution_data.csv /user/$MY_USERNAM
 hadoop fs -test -d /user/$MY_USERNAME/vic-output && hadoop fs -rm -r /user/$MY_USERNAME/vic-output
 
 echo "Running spark-submit"
-spark-submit --master yarn --deploy-mode cluster main.py $MY_USERNAME --sample
-# spark-submit --master yarn --deploy-mode cluster --py-files main.py main.py $MY_USERNAME
+#spark-submit --master yarn --deploy-mode cluster main.py $MY_USERNAME --sample
+spark-submit --master yarn main.py $MY_USERNAME --sample
 
 # Print the output (its saved in hdfs)
 hadoop fs -cat /user/$MY_USERNAME/vic-output/part-00000
