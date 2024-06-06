0a77c30ed04e5fe4a105c0e1f3575e13e4afdbb1
Nicholas Hemphill McCormick <mccormnich@co246a-1.ecs.vuw.ac.nz>
2024-06-06 23:14:25 +1200

    Temp sloppy ignore trigrams and only run dt

--- run_spark.sh
+++ run_spark.sh
@@ -8,8 +8,14 @@ hadoop fs -put Gungor_2018_VictorianAuthorAttribution_data.csv /user/$MY_USERNAM
 hadoop fs -test -d /user/$MY_USERNAME/vic-output && hadoop fs -rm -r /user/$MY_USERNAME/vic-output
 
 echo "Running spark-submit"
-#spark-submit --master yarn --deploy-mode cluster main.py $MY_USERNAME --sample
-spark-submit --master yarn main.py $MY_USERNAME --sample
+#spark-submit --master yarn --deploy-mode cluster main.py $MY_USERNAME 
+spark-submit --master yarn main.py $MY_USERNAME 
 
 # Print the output (its saved in hdfs)
-hadoop fs -cat /user/$MY_USERNAME/vic-output/part-00000
+hadoop fs -cat /user/$MY_USERNAME/vic-output/logs/part-00000
+echo "--"
+hadoop fs -cat /user/$MY_USERNAME/vic-output/df-0/feat_df_0.csv
+echo "--"
+hadoop fs -cat /user/$MY_USERNAME/vic-output/df-1/feat_df_1.csv
+echo "--"
+hadoop fs -cat /user/$MY_USERNAME/vic-output/df-2/feat_df_2.csv
